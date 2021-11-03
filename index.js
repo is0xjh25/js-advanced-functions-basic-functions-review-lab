d7d9ce5c2a8823ff6c34acabd7d79b350c2fea0d
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-03 11:54:05 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function saturdayFun(a="roller-skate") {
+	return `This Saturday, I want to ${a}!`;
+}
+
+function mondayWork(a="go to the office") {
+	return `This Monday, I will ${a}.`;
+}
+
+function wrapAdjective(y="*") {
+	return function (x="sepcial") {
+		return `You are ${y}${x}${y}!`;
+	} 
+}
+
+let Calculator= {
+	add(x, y) {
+		return x + y;
+	},
+	subtract(x, y) {
+		return x - y;
+	},
+	multiply(x, y) {
+		return x * y;
+	},
+	divide(x, y) {
+		return x / y;
+	}
+};
+
+function actionApplyer(s, fns) {
+	if (fns.length === 0) {
+		return s;
+	} else {
+		let a = 13;
+		for (let i = 0; i < fns.length; i++) {
+			a = fns[i](a);
+		}
+		return a;		
+	}
+
+}
